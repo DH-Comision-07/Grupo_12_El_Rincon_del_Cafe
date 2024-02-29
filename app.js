39f2de43aca41d8fba4eee3e875f98743420f70b
NereaArin <nerea.arin@icloud.com>
2024-02-29 11:43:00 -0300

    fixed: modifico nombre de los archivos a productos

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get("/productDetail", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+app.get('/products', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/products.html'));
 });
 
-app.get("/productCart", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+app.get('/productCart', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
-app.get("/h&f", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/h&f.html"));
+app.get('/h&f', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/h&f.html'));
 });
 
 app.listen(5050, () => {
-  console.log("Servidor corriendo en 5050");
+  console.log('Servidor corriendo en 5050');
 });
