fec3cb9cba55793208d8a1f4d50358753a49126e
NereaArin <nerea.arin@icloud.com>
2024-03-10 17:45:25 -0300

    feat: implemento rutas y controlador templates engine

--- src/app.js
+++ src/app.js
@@ -1,53 +1,46 @@
+/* Require */
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5050;
+const mainRoutes = require('./routes/main.routes');
 
+/* USE */
 app.use(express.static('public'));
+app.use('/', mainRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/main/index.html'));
-});
+/* SET */
+app.set('view engine', 'ejs');
 
-app.get('/products', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/products.html'));
-});
-app.get('/product-detail', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/productDetail.html'));
-});
-
-app.get('/product-cart', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/productCart.html'));
-});
-
-app.get('/register', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/users/register.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/users/login.html'));
-});
-app.get('/h&f', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/h&f.html'));
-});
-app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/main/contact.html'));
-});
-app.get('/about-us', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/main/aboutUs.html'));
-});
-app.get('/product-edition', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/products/productEdition.html'));
-});
-app.get('/product-generation', (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, './views/products/productGeneration.html')
-  );
-});
-app.get('/suscripcion', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/main/suscripcion.html'));
-});
-
+/* PORT */
+const port = process.env.PORT || 5050;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
+
+// /* Products */
+// app.get('/products', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/products/products.ejs'));
+// });
+// app.get('/product-detail', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/products/productDetail.ejs'));
+// });
+
+// app.get('/product-cart', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/products/productCart.ejs'));
+// });
+// app.get('/product-edition', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/products/productEdition.ejs'));
+// });
+// app.get('/product-generation', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/products/productGeneration.ejs'));
+// });
+
+// /* Users */
+
+// app.get('/register', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/users/register.ejs'));
+// });
+
+// app.get('/login', (req, res) => {
+//   res.render(path.resolve(__dirname, './views/users/login.ejs'));
+// });
