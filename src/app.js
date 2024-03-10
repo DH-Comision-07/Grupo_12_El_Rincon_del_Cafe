61a6b20d45c6065cfb4cb05a855b66f4be4b883b
NereaArin <nerea.arin@icloud.com>
2024-03-10 18:05:11 -0300

    feat: implemento rutas y controladores de products

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mainRoutes = require('./routes/main.routes');
+const productsRoutes = require('./routes/products.routes');
 
 /* USE */
 app.use(express.static('public'));
 app.use('/', mainRoutes);
+app.use('/products', productsRoutes);
 
 /* SET */
 app.set('view engine', 'ejs');
@@ -16,24 +18,6 @@ const port = process.env.PORT || 5050;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
-
-// /* Products */
-// app.get('/products', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/products/products.ejs'));
-// });
-// app.get('/product-detail', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/products/productDetail.ejs'));
-// });
-
-// app.get('/product-cart', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/products/productCart.ejs'));
-// });
-// app.get('/product-edition', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/products/productEdition.ejs'));
-// });
-// app.get('/product-generation', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/products/productGeneration.ejs'));
-// });
 
 // /* Users */
 
---
+++ src/controllers/productsController.js
@@ -0,0 +1,33 @@
+/* Require */
+const express = require('express');
+const path = require('path');
+
+const productsController = {
+  products: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/products.ejs')
+    );
+  },
+  productDetail: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/productDetail.ejs')
+    );
+  },
+
+  cart: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/productCart.ejs')
+    );
+  },
+  edition: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/productEdition.ejs')
+    );
+  },
+  create: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/productGeneration.ejs')
+    );
+  },
+};
+module.exports = productsController;
