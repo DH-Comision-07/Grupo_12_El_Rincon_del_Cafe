5fc53045ceaf846af50cedb5e3c1d233fc86d254
NereaArin <nerea.arin@icloud.com>
2024-03-10 18:15:49 -0300

    feat: agrego rutas y controladores templates engine

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 /* Require */
 const express = require('express');
-const path = require('path');
 const app = express();
 const mainRoutes = require('./routes/main.routes');
 const productsRoutes = require('./routes/products.routes');
+const usersRoutes = require('./routes/users.routes');
 
 /* USE */
 app.use(express.static('public'));
 app.use('/', mainRoutes);
 app.use('/products', productsRoutes);
+app.use('/users', usersRoutes);
 
 /* SET */
 app.set('view engine', 'ejs');
@@ -18,13 +19,3 @@ const port = process.env.PORT || 5050;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
-
-// /* Users */
-
-// app.get('/register', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/users/register.ejs'));
-// });
-
-// app.get('/login', (req, res) => {
-//   res.render(path.resolve(__dirname, './views/users/login.ejs'));
-// });
