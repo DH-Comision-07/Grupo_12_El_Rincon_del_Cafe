9f8b2d1c892d2f2575caac21954f9c00c0faf798
NereaArin <nerea.arin@icloud.com>
2024-03-21 11:19:56 -0300

    feat: se establecer rutas del controlador de users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 /* Require */
 const express = require('express');
 const path = require('path');
+const usersService = require('../data/userService');
 
 const usersController = {
   register: (req, res) => {
@@ -10,16 +11,30 @@ const usersController = {
     return res.render(path.resolve(__dirname, '../views/users/login.ejs'));
   },
   userprofile: (req, res) => {
-    return res.render(path.resolve(__dirname, '../views/users/userProfile.ejs'));
+    return res.render(
+      path.resolve(__dirname, '../views/users/userProfile.ejs')
+    );
   },
   adminprofile: (req, res) => {
-    return res.render(path.resolve(__dirname, '../views/users/adminProfile.ejs'));
+    return res.render(
+      path.resolve(__dirname, '../views/users/adminProfile.ejs')
+    );
   },
   productManagement: (req, res) => {
-    return res.render(path.resolve(__dirname, '../views/users/productManagement.ejs'));
+    return res.render(
+      path.resolve(__dirname, '../views/users/productManagement.ejs')
+    );
   },
   shoppingHistory: (req, res) => {
-    return res.render(path.resolve(__dirname, '../views/users/shoppingHistory.ejs'));
+    return res.render(
+      path.resolve(__dirname, '../views/users/shoppingHistory.ejs')
+    );
+  },
+  dashboard: (req, res) => {
+    const users = usersService.getAll();
+    return res.render('../views/users/userDashboard', {
+      users: users,
+    });
   },
 };
 module.exports = usersController;
