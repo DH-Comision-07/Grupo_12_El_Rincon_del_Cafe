109eb95057bebd6ce498a2814d1d87b193d36ad0
NereaArin <nerea.arin@icloud.com>
2024-03-20 23:47:39 -0300

    Refactor productsService.js

--- src/data/productsService.js
+++ src/data/productsService.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-let products = require("../data/productsDataBase.json");
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const fs = require('fs');
+const path = require('path');
+let products = require('../data/productsDataBase.json');
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
 let productsService = {
-    products: products,
-  
-    getAll: function () {
-      return this.products;
-    },
-  
-    getOneBy: function (id) {
-      return this.products.find((product) => product.id == id);
-    },
-}
+  products: products,
+
+  getAll: function () {
+    return this.products;
+  },
+
+  getOneBy: function (id) {
+    return this.products.find((product) => product.id == id);
+  },
+};
 
 module.exports = productsService;
