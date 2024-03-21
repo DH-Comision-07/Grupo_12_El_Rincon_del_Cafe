399cb3409293acf5f5b50abe267d2476b077aca8
NereaArin <nerea.arin@icloud.com>
2024-03-20 22:24:38 -0300

    feat: agrego ruta para productDashboard

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,49 +1,56 @@
 /* Require */
-const express = require("express");
-const path = require("path");
-const productsService = require("../data/productsService");
+const express = require('express');
+const path = require('path');
+const productsService = require('../data/productsService');
 
 const productsController = {
   products: (req, res) => {
-    return res.render("../views/products/products", {
+    return res.render('../views/products/products', {
       products: productsService.getAll(),
     });
   },
   productDetail: (req, res) => {
-  const id = req.params.id;
-  const product = productsService.getOneBy(id);
-  if (product) {
-    return res.render('../views/products/productDetail', {product: product});
-  };
-},
+    const id = req.params.id;
+    const product = productsService.getOneBy(id);
+    if (product) {
+      return res.render('../views/products/productDetail', {
+        product: product,
+      });
+    }
+  },
   category: (req, res) => {
     const category = req.params.category;
     const allProducts = productsService.getAll();
     const filteredProducts = allProducts.filter(
       (product) => product.category === category
     );
-    return res.render("../views/products/products", {
+    return res.render('../views/products/products', {
       products: filteredProducts,
     });
   },
   cart: (req, res) => {
     return res.render(
-      path.resolve(__dirname, "../views/products/productCart.ejs")
+      path.resolve(__dirname, '../views/products/productCart.ejs')
     );
   },
   edition: (req, res) => {
     return res.render(
-      path.resolve(__dirname, "../views/products/productEdition.ejs")
+      path.resolve(__dirname, '../views/products/productEdition.ejs')
     );
   },
   create: (req, res) => {
     return res.render(
-      path.resolve(__dirname, "../views/products/productGeneration.ejs")
+      path.resolve(__dirname, '../views/products/productGeneration.ejs')
     );
   },
   cat: (req, res) => {
     return res.render(
-      path.resolve(__dirname, "../views/products/productCategory.ejs")
+      path.resolve(__dirname, '../views/products/productCategory.ejs')
+    );
+  },
+  dashboard: (req, res) => {
+    return res.render(
+      path.resolve(__dirname, '../views/products/productDashboard.ejs')
     );
   },
 };
