a82fab3826e1daacfca2c0f0d014a7e904345028
NereaArin <nerea.arin@icloud.com>
2024-03-22 14:28:19 -0300

    feat: agrego y completo service para que conecte con vista de borrar productos

--- src/data/productsService.js
+++ src/data/productsService.js
@@ -13,6 +13,17 @@ let productsService = {
   getOneBy: function (id) {
     return this.products.find((product) => product.id == id);
   },
+  saveProducts: function (products) {
+    const productsDBPath = path.join(__dirname, './productsDataBase.json');
+    fs.writeFileSync(productsDBPath, JSON.stringify(products, null, 2));
+  },
+  deleteProduct: function (id) {
+    console.log(`Deleting product with id ${id}`);
+    const products = this.getAll();
+    const nonDeletedProducts = products.filter((product) => product.id != id);
+    this.saveProducts(nonDeletedProducts);
+    return nonDeletedProducts;
+  },
 };
 
 module.exports = productsService;
