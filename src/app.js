4dbc9351672fcfb890c62159a36950af358e4a50
Facundo Dome Lobo <fackundodiez@gmail.com>
2024-05-15 11:54:06 +0200

    ds

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 /* Require */
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mainRoutes = require('./routes/main.routes');
-const productsRoutes = require('./routes/products.routes');
-const usersRoutes = require('./routes/users.routes');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const cookies = require('cookie-parser');
-const isLogged = require('./middlewares/isLogged');
+const path = require("path");
+const mainRoutes = require("./routes/main.routes");
+const productsRoutes = require("./routes/products.routes");
+const usersRoutes = require("./routes/users.routes");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const cookies = require("cookie-parser");
+const isLogged = require("./middlewares/isLogged");
+const dotenv = require("dotenv").config();
 
+console.log(process.env.DB_USER);
 /* USE */
-app.use(express.static('public'));
-app.use(methodOverride('_method'));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 app.use(
   session({
-    secret: 'grupo12',
+    secret: "grupo12",
     resave: false,
     saveUninitialized: false,
   })
@@ -24,13 +26,13 @@ app.use(cookies());
 app.use(isLogged);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', mainRoutes);
-app.use('/products', productsRoutes);
-app.use('/users', usersRoutes);
+app.use("/", mainRoutes);
+app.use("/products", productsRoutes);
+app.use("/users", usersRoutes);
 
 /* SET */
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 /* PORT */
 const port = process.env.PORT || 5050;
