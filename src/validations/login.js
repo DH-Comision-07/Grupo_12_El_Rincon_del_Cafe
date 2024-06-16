5452582fab288ed9c3eb4abcbe842bc68038bf1f
Facundo Dome Lobo <fackundodiez@gmail.com>
2024-06-16 19:14:39 +0200

    fix: validaciones del front login-register

--- src/validations/login.js
+++ src/validations/login.js
@@ -1,17 +1,17 @@
-const { body } = require('express-validator');
-const path = require('path');
+const { body } = require("express-validator");
+const path = require("path");
 
 module.exports = [
-  body('email')
+  body("email")
     .notEmpty()
-    .withMessage('Debes ingresar un email')
+    .withMessage("Debes ingresar un email")
     .bail()
     .isEmail()
-    .withMessage('Debes ingresar un email válido'),
-  body('password')
+    .withMessage("Debes ingresar un email válido"),
+  body("password")
     .notEmpty()
-    .withMessage('Debes completar la contraseña')
+    .withMessage("Debes completar la contraseña")
     .bail()
     .isLength({ min: 8 })
-    .withMessage('La contraseña debe tener como mínimo 8 caracteres'),
+    .withMessage("La contraseña debe tener como mínimo 8 caracteres"),
 ];
